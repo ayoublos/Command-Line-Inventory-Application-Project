f406a6f92a6cbd04d1d22174fcc40b0936650e44
ayoublos <abelibel@pursuit.org>
2024-06-13 21:44:33 -0700

    finished 90 percent of the project

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const{readFileFunction,writeFileFunction}=require(`./src/purchaseController`)
 
-const{create}=require(`./src/helpers`)
+const{clear,create,destroy,show,index, update}=require(`./src/helpers`)
 
 function run(){
     const purchases=readFileFunction(`data`,`purchases.json`)
     const action=process.argv[2];
     const purchaseName=process.argv[3];
+    const purchaseId=process.argv[3];
+    const name=process.argv[4];
+    const price=process.argv[5]
     const priceInCents=process.argv[4]
-   let updatedList=[]
-   let writeToFile=false;
 
-   switch(action){
-    case `create`:
-        updatedList=create(purchases,purchaseName,priceInCents)
-        break;
-    case `update`:
-        break;
-    case `show`:
-        break;
+    let updatedList=[]
+    let writeToFile=false;
+    switch(action){
+        case `create`: 
+            updatedList=create(purchases,purchaseName,priceInCents)
+            writeToFile=true;
+            break;
+            case `update`:
+                updatedList=update(purchases,purchaseId,name,price)
+                writeToFile=true;
+            break;
+            case `show`:
+            console.log(show(purchases,purchaseId))
+            writeToFile=false;
+            break;
     case `index`:
+        console.log(index(purchases))
+        writeToFile=false;
         break;
     case `destroy`:
+        updatedList=destroy(purchases,purchaseId)
+        writeToFile=true
         break;
+    case `clear`:
+        updatedList=clear()
+        writeToFile=true
+        break;
+   }
+   if(writeToFile){
+    writeFileFunction(`data`,`purchases.json`,updatedList)
    }
 }
 run()
--- src/helpers.js
+++ src/helpers.js
@@ -3,12 +3,33 @@ const{nanoid}=require(`nanoid`)
 function create(purchases,name,price){
 const newPurchase={
     id:nanoid(10),
-    name:name,
-    priceInCents:price,
+    name:name||`no designation`,
+    priceInCents:price||`0`,
     inStock:false,
 }
 purchases.push(newPurchase)
 return purchases
 }
+function destroy(purchases,id){
+    return purchases.filter(purchase=>purchase.id!==id)
 
-module.exports={create}
+}
+function show(purchases,id){
+    let string=JSON.stringify(purchases.find(purchase=>purchase.id===id))
+    return string.slice(1,string.length-1)
+}
+function index(purchases){
+    return purchases.map(purchase=>`id:${purchase.id}$ name:${purchase.name}priceInCents:${purchase.priceInCents} in stock:${purchase.inStock}`).join(`\n`)
+}
+function update(purchases,id,name,price){
+    let index=purchases.findIndex(purchase=>purchase.id===id);
+    purchases[index].id=id;
+    purchases[index].name=name;
+    purchases[index].priceInCents=price
+    return purchases
+}
+function clear(){
+    return []
+}
+
+module.exports={clear,create,destroy,show,index,update}
